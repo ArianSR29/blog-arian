3f9f7fd1ceeffd61bbc631e2fe4054ec23f8fd9e
Rodrigo Arenas González <arenasgonzalezrodrigo@gmail.com>
2018-11-22 11:59:35 -0600

    hacienbdo configuración, e instalando la base d edatos

--- blogenv/mysite/settings.py
+++ blogenv/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hti+g6!#qqg0ux(=@#-l&uw6)7d4qou-h6qs#ej01_ff4%@+8!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', ' . arianblog.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT-6'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
